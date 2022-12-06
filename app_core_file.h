68f22fba39ffbae042b46bfa46531903f0366d23
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-12-06 22:04:58 +0100

    file

--- app_core_file.h
+++ app_core_file.h
@@ -28,20 +28,20 @@ int8_t myStrcmp(const char* __s1, const char* __s2)
     return res > 0 ? 1 : -1;
 }
 
-bool nextFile(char* filename, const char* folder, const char* current, int8_t direction)
+bool nextFile(char* filename, const char* folder, const char* current, int8_t direction, uint8_t len = 255)
 {
     bool next = false;
     DIR* x = opendir(folder);
     if (x != NULL) {
         struct dirent* directory;
 
-        char cur[256];
-        strncpy(cur, current, 256);
-        strncpy(filename, cur, 256);
+        char cur[255];
+        strncpy(cur, current, len);
+        strncpy(filename, cur, len);
         while ((directory = myReaddir(x)) != NULL) {
             if (myStrcmp(directory->d_name, cur) == direction
                 && (strcmp(filename, cur) == 0 || myStrcmp(directory->d_name, filename) == direction * -1)) {
-                strncpy(filename, directory->d_name, 256);
+                strncpy(filename, directory->d_name, len);
                 next = true;
             }
         }
@@ -50,7 +50,7 @@ bool nextFile(char* filename, const char* folder, const char* current, int8_t di
     return next;
 }
 
-bool firstFile(char* filename, const char* folder)
+bool firstFile(char* filename, const char* folder, uint8_t len = 255)
 {
     bool initialized = false;
     DIR* x = opendir(folder);
@@ -59,7 +59,7 @@ bool firstFile(char* filename, const char* folder)
         while ((directory = myReaddir(x)) != NULL) {
             // printf("%s <> %s = %d\n", directory->d_name, filename, myStrcmp(directory->d_name, filename));
             if (!initialized || myStrcmp(directory->d_name, filename) == -1) {
-                strncpy(filename, directory->d_name, 256);
+                strncpy(filename, directory->d_name, len);
                 initialized = true;
             }
         }
